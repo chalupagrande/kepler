5aea1773f0ef7a53bc858a0ef1fad16d13fd1641
Jamie Skinner <jamie@ninthmind.com>
2016-09-21 15:11:21 -0500

    registering elements with kepler

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var browserSync = require('browser-sync');
-var plumber = require('gulp-plumber')
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
 
-gulp.task('serve', ['compile'], function(){
+// gulp.task('serve', ['compile'], function(){
+gulp.task('serve', function(){
   browserSync.init({
     server: {
       baseDir:'./'
     }
   })
-  gulp.watch(['src/**/*.js', '**/*.html'] , ['compile'])
+  // gulp.watch(['src/**/*.js', '**/*.html'] , ['compile'])
   gulp.watch(['src/**/*.js', '**/*.html']).on('change', browserSync.reload)
 })
 
 gulp.task('compile', function(){
-  return gulp.src('src/**/*.js')
+  return gulp.src(['src/**/*.js','*.html'])
              .pipe(plumber())
              .pipe(babel({
                presets: ['es2015']
--- src/kepler.js
+++ src/kepler.js
@@ -1,6 +1,61 @@
-var app = document.getElementById('app')
-var imports;
-window.addEventListener('HTMLImportsLoaded', function(e) {
-  // all imports loaded
+class Kepler {
+  /*
+    opts: {
+      element: CSS SELECTOR --id of component template
+      name: STRING -- string for component name
+      expose: BOOLEAN -- expose the component to the DOM
+      createdCallback: FUNC -- function to be called when element is created
+    }
+  */
+  constructor(opts){
+    let importee = (document._currentScript || document.currentScript).ownerDocument;
+    var template = importee.querySelector(opts.element)
 
-});
+    let node = Object.create(HTMLElement.prototype)
+    //createdCallback
+    node.createdCallback = function(){
+      debugger
+      let clone = document.importNode(template.content, true)
+      let root = this.createShadowRoot()
+      root.appendChild(clone)
+      console.log("ROOT LOG - KEP", root)
+    }
+
+    //attachedCallback
+    node.attachedCallback = function(){
+      console.log('kepler attached', this)
+    }
+    //detachedCallback
+    node.detachedCallback = function(){
+
+    }
+    //attributeChangedCallback
+    node.attributeChangedCallback = function(){
+
+    }
+
+    
+    node.foo = function(){
+      console.log('fooooooo')
+    }
+    
+    let register = function(){
+      
+      var jamie = document.registerElement(opts.name, {
+        prototype: node
+      })
+      console.log('thing', this)
+    }
+    let k = register.bind(window)
+    k()
+  
+    
+    this.node = node
+  }
+  method(){
+    console.log('KEPLER')
+  }
+}
+
+/* Helpers 
+~~~~~~~~~~~~~~ */
